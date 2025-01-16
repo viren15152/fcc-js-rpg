1ceb641af3919ca95ea621b9e0da0b69eb1c4422
Viren <144032304+viren15152@users.noreply.github.com>
2025-01-16 16:21:08 +0000

    completed the sellWeapon function

--- script.js
+++ script.js
@@ -106,16 +106,25 @@ function buyWeapon() {
 }
 
 function sellWeapon() {
-    if (inventory.length > 1){
-      
+    if (inventory.length > 1) {
+      gold += 15;
+      goldText.innerText = gold;
+      let currentWeapon = inventory.shift();
+      text.innerText = "You sold a " + currentWeapon + ".";
+      text.innerText += " In your inventory you have: " + inventory;
+    } else {
+      text.innerText = "Don't sell your only weapon!";
     }
-  
   }
-
+  
   function fightSlime() {
-
+  
   }
   
   function fightBeast() {
   
+  }
+  
+  function fightDragon() {
+    console.log("Fighting dragon.");
   }
